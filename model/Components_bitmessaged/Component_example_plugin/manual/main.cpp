25b3baca61b2b2c9d439949345f8f41710875acf
Thomas Spitzer <projectleiter@astade.de>
2014-04-22 13:29:25 +0200

    changed makefiles for the shared objects

--- model/Components_bitmessaged/Component_example_plugin/manual/main.cpp
+++ model/Components_bitmessaged/Component_example_plugin/manual/main.cpp
@@ -1,12 +1,15 @@
+#include <stdio.h>
 #include "knowledge.h"
 
 extern "C" {
 void init(data::knowledge& data)
 {
+    printf("example plugin initialized\n");
 }
 
 void shutdown()
 {
+    printf("example plugin shut down\n");
 }
 
 }
