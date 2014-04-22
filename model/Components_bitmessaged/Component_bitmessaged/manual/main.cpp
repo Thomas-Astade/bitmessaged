124e18e0324cd8cb2c60f294df617a65d9ab92be
Thomas Spitzer <projectleiter@astade.de>
2014-04-22 10:48:45 +0200

    new option -i

--- model/Components_bitmessaged/Component_bitmessaged/manual/main.cpp
+++ model/Components_bitmessaged/Component_bitmessaged/manual/main.cpp
@@ -9,6 +9,13 @@ void print_usage()
     printf("\nOptions:\n");
     printf("--help              Display this information\n");
     printf("-p, --plugin        plugin to load.\n");
+    printf("-i, --initial       ip:port of the initial client connection.\n");
+    printf("                    (you may have multiple addresses).\n");
+}
+
+bool create_outgoing_connection(const char* arg)
+{
+    return true;
 }
 
 int main(int argc, char *argv[]) {
@@ -16,17 +23,25 @@ int main(int argc, char *argv[]) {
     static struct option long_options[] = {
         {"help"       ,  no_argument,        0,  'h' },
         {"plugin"     ,  optional_argument,  0,  'p' },
+        {"initial"    ,  required_argument,  0,  'i' },
         {0,          0,                      0,   0  }
     };
 
     int long_index =0;
     int opt;
 
-    while ((opt = getopt_long(argc, argv, "p:", long_options, &long_index )) != -1) {
+    while ((opt = getopt_long(argc, argv, "p:i:", long_options, &long_index )) != -1) {
         switch (opt) {
         case 'h':
             print_usage();
             exit(EXIT_SUCCESS);
+            break;
+        case 'i':
+            if (!create_outgoing_connection(optarg))
+            {
+                printf("ERROR: -i <addr>:<port> e.g.: -i 127.0.0.1:8444\n");
+                exit (EXIT_FAILURE);
+            }
             break;
         default:
             break;
