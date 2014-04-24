4b51109c573b34531ef431cfaeeef815c11b52db
Thomas Spitzer <projectleiter@astade.de>
2014-04-24 09:51:21 +0200

    add a dump function (and some renames)

---
+++ model/Package_bitmessaged/Package_protocol/classes/class_Payload/operations/operation_dump/code.cpp
@@ -0,0 +1,46 @@
+//~~ void dump() [CPayload] ~~
+const void* mem = operator*();
+unsigned int len = size();
+unsigned int i, j;
+const unsigned int columns = 16;
+
+for(i = 0; i < len + ((len % columns) ? (columns - len % columns) : 0); i++)
+{
+    /* print offset */
+    if(i % columns == 0)
+    {
+        printf("0x%06x: ", i);
+    }
+
+    /* print hex data */
+    if(i < len)
+    {
+        printf("%02x ", 0xFF & ((char*)mem)[i]);
+    }
+    else /* end of block, just aligning for ASCII dump */
+    {
+        printf("   ");
+    }
+
+    /* print ASCII dump */
+    if(i % columns == (columns - 1))
+    {
+        for(j = i - (columns - 1); j <= i; j++)
+        {
+            if(j >= len) /* end of block, not really printing */
+            {
+                putchar(' ');
+            }
+            else if(isprint(((char*)mem)[j])) /* printable char */
+            {
+                putchar(0xFF & ((char*)mem)[j]);        
+            }
+            else /* other char */
+            {
+                putchar('.');
+            }
+        }
+        putchar('\n');
+    }
+}
+
--- model/Package_bitmessaged/Package_protocol/classes/class_Payload/prolog.cpp
+++ model/Package_bitmessaged/Package_protocol/classes/class_Payload/prolog.cpp
@@ -1,3 +1,5 @@
 #include <netinet/in.h>
+#include <stdio.h>
+#include <ctype.h>
 
 #define VECTOR (std::tr1::shared_ptr<std::vector<uint8_t> >::operator*())
