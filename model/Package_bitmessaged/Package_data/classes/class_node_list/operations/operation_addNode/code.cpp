39bc333b7766852c2fc0c9acd437493a2968a71e
Thomas Spitzer <t.spitzer@consulting.astade.de>
2014-04-29 13:27:09 +0200

    can insert nodes in node list

--- model/Package_bitmessaged/Package_data/classes/class_node_list/operations/operation_addNode/code.cpp
+++ model/Package_bitmessaged/Package_data/classes/class_node_list/operations/operation_addNode/code.cpp
@@ -1,2 +1,32 @@
 //~~ bool addNode(const std::string& addr) [node_list] ~~
-RETURN(false);
+size_t pos = addr.find(':');
+
+if (pos == std::string::npos)
+    RETURN(false);
+
+
+
+struct sockaddr_in serv_addr;
+memset(&serv_addr, '0', sizeof(serv_addr)); 
+
+serv_addr.sin_family = AF_INET;
+int port = atoi(addr.substr(pos+1).c_str());
+serv_addr.sin_port = htons(port);
+
+if (inet_pton(AF_INET, addr.substr(0,pos).c_str(), &serv_addr.sin_addr)==0)
+{
+    RETURN(false);
+}
+
+char buffer[100];
+if (inet_ntop(AF_INET, &serv_addr, buffer, sizeof(buffer)) && serv_addr.sin_port)
+{
+    char buffer2[100];
+    sprintf(buffer2,"%s:%d",buffer,port);
+    known_nodes[buffer2] = node_info(serv_addr);
+    RETURN(true);
+}
+else
+{
+    RETURN(false);
+}
---
+++ model/Package_bitmessaged/Package_data/classes/class_node_list/prolog.cpp
@@ -0,0 +1,4 @@
+#include <string.h>
+#include <arpa/inet.h> 
+#include <stdio.h>      /* printf, fgets */
+#include <stdlib.h>     /* atoi */
