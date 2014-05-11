f14c38c0999543dca38810bcdc6730270ecb6b0f
Thomas Spitzer <t.spitzer@consulting.astade.de>
2014-05-11 16:08:17 +0200

    some optimising in download order

--- model/Package_bitmessaged/classes/class_OutBitmessageLogic/operations/operation_handleObject/code.cpp
+++ model/Package_bitmessaged/classes/class_OutBitmessageLogic/operations/operation_handleObject/code.cpp
@@ -1,8 +1,7 @@
 //~~ void handleObject(ACF_Message* event) [OutBitmessageLogic] ~~
 protocol::object* anObject = (protocol::object*)event->Data;
 
-if (*VectorsToRequest.begin() == anObject->getVector())
-    theKnowledge.addObject(*anObject);
+theKnowledge.addObject(*anObject);
 
 if (!VectorsToRequest.empty())
     ACF_sendMessage(MessageReceiver(),MessageReceiver(),ev_request,0);
--- model/Package_bitmessaged/classes/class_OutBitmessageLogic/operations/operation_requestObjects/code.cpp
+++ model/Package_bitmessaged/classes/class_OutBitmessageLogic/operations/operation_requestObjects/code.cpp
@@ -6,6 +6,25 @@ if (VectorsToRequest.empty())
     return;
 
 protocol::getdata aGet;
-aGet.insert(*VectorsToRequest.begin());
+
+if (VectorsToRequest.size() > 250)
+{
+    std::set<protocol::inventory_vector>::iterator it = VectorsToRequest.begin();
+    unsigned int step = (rand() % 200) + 3;
+    
+    while (step--)
+        it++;
+        
+    aGet.insert(*it);
+    
+    if (!theKnowledge.doWeNeed(*it))
+    {
+        VectorsToRequest.erase(*it);
+        ACF_sendMessage(MessageReceiver(),MessageReceiver(),ev_request,0);
+        return;
+    }
+}
+else
+    aGet.insert(*VectorsToRequest.begin());
 
 ACF_sendMessage(MessageReceiver(),toSocket,ev_getdata,new protocol::Payload(aGet.getPayload()));
