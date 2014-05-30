d11eaf1e0196d6af90ed9582e4bbfc93ecc7ccde
Thomas Spitzer <t.spitzer@consulting.astade.de>
2014-05-30 20:13:42 +0200

    time limit for incomming sockets

--- model/Package_bitmessaged/classes/class_InSocketHandler/operations/operation_internalThread/code.cpp
+++ model/Package_bitmessaged/classes/class_InSocketHandler/operations/operation_internalThread/code.cpp
@@ -1,3 +1,14 @@
 //~~ void internalThread() [InSocketHandler] ~~
+struct timeval timeout;
+timeout.tv_sec = 1;
+timeout.tv_usec = 0;
+
+if (setsockopt (socketfd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout,sizeof(timeout)) < 0)
+{
+    ACF_sendMessage(MessageReceiver(),MessageReceiver(),ev_error,0);
+    ACF_sendMessage(MessageReceiver(),MessageReceiver(),ev_disconnected,0);
+    return;
+}
+
 ACF_sendMessage(MessageReceiver(),MessageReceiver(),ev_connected,0);
 threadloop();
