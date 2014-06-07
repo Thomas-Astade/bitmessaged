bd09ae31699c4c73da6a4ac2656f20176db1fb79
Thomas Spitzer <t.spitzer@consulting.astade.de>
2014-06-07 12:18:03 +0200

    semaphore use in database.save

--- model/Package_bitmessaged/Package_data/classes/class_knowledge/operations/operation_save/code.cpp
+++ model/Package_bitmessaged/Package_data/classes/class_knowledge/operations/operation_save/code.cpp
@@ -1,6 +1,11 @@
 //~~ void save() [knowledge] ~~
+while (sem_wait(&mySemaphore))
+    ;
+    
 json_spirit::mObject obj;
 obj["known_nodes"] = known_node_list.get_info();
+
+sem_post(&mySemaphore);
 
 std::string filename = std::string(getenv ("HOME")) + "/" + ".bitmessaged.json";
 std::ofstream os( filename.c_str() );
