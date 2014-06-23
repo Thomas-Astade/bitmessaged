d7f66344ab42df2c35b847ccf84f1d8030b93ae8
thomas <thomas@L049E025.(none)>
2014-06-23 07:56:40 +0200

    fixed the uptime value

--- model/Components_bitmessaged/Component_mongoose_plugin/manual/main.cpp
+++ model/Components_bitmessaged/Component_mongoose_plugin/manual/main.cpp
@@ -95,7 +95,7 @@ static void overview(struct mg_connection *conn) {
     mg_printf_data(conn,"<table border=\"1\">\n");
     unsigned int t = database->getHeartbeat();
     mg_printf_data(conn,"<tr><td>uptime</td><td>%d days %d:%02d:%02d hours</td></tr>\n",
-        t/(24*60*60),t/(60*60) % (24*60*60),t/(60) % (60*60),t % 60);
+        t/(24*60*60),t % (24*60*60) / (60*60) ,t % (60*60) / (60),t % 60);
 
     mg_printf_data(conn,"<tr><td>known nodes</td><td>%d</td></tr>\n",database->getNodeCount());
     std::vector<data::node_info> nodes = database->getNodesToProvideToOthers();
