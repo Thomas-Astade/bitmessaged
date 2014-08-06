9f5435584f8ba36b3c55e310d4d06eeb9a6148ca
Thomas Spitzer <projektleiter@astade.de>
2014-08-06 12:52:08 +0200

    made a hash function for objects that has identical result for V2 and V3 objects

---
+++ model/Package_bitmessaged/Package_protocol/classes/class_object/operations/operation_VHYEDDJF/code.cpp
@@ -0,0 +1,14 @@
+//~~ std::string getContentHash() [object] ~~
+uint8_t hash[MD5_DIGEST_LENGTH];
+
+if ((myType == protocol::message::object) && myPayload.size() > 20)
+{
+    MD5(*myPayload+20, myPayload.size()-20, hash);
+    RETURN(std::string((char*)hash,MD5_DIGEST_LENGTH));
+} else if ((myType == protocol::message::object) && myPayload.size() > 16)
+{
+    MD5(*myPayload+16, myPayload.size()-16, hash);
+    RETURN(std::string((char*)hash,MD5_DIGEST_LENGTH));
+}
+
+RETURN(std::string());
--- model/Package_bitmessaged/Package_protocol/classes/class_object/prolog.cpp
+++ model/Package_bitmessaged/Package_protocol/classes/class_object/prolog.cpp
@@ -1,3 +1,5 @@
 #include <string>
 #include <ctime>
 #include <stdio.h>
+#include <openssl/md5.h>
+
