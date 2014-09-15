9fb566c3e09eeef927c4029ab91a5da51d581fd4
Thomas Spitzer <projektleiter@astade.de>
2014-09-15 11:37:08 +0200

    the genPOW works

--- model/Components_bitmessaged/Component_YMOECETA/manual/main.cpp
+++ model/Components_bitmessaged/Component_YMOECETA/manual/main.cpp
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <getopt.h>
+#include <string>
 
 
 void print_usage()
@@ -19,35 +20,35 @@ void print_usage()
     printf("-i, --hash               the initial hash of the massage in hex.\n");
 }
 
-unsigned int getTimeTick(void)
+uint64_t htonll(uint64_t value)
 {
-    struct timeval time;
-    gettimeofday(&time, NULL);
-    return time.tv_sec * 1000000 + time.tv_usec;
+    uint32_t high_part = htonl((uint32_t)(value >> 32));
+    uint32_t low_part = htonl((uint32_t)(value & 0xFFFFFFFFLL));
+    return (((uint64_t)low_part) << 32) | high_part;
 }
 
-void doMHash()
+uint64_t doMHash(uint64_t target, char* initialHash)
 {
-    uint8_t initialHash[SHA512_DIGEST_LENGTH];
     uint8_t buffer[8+SHA512_DIGEST_LENGTH];
     
-    for (unsigned int i = 0; i < SHA512_DIGEST_LENGTH; i++)
-        initialHash[i] = rand();
-
     memcpy(&buffer[8], initialHash, SHA512_DIGEST_LENGTH);
     
+    uint64_t trialValue = (uint64_t)0xffffffffffffffff;
     uint64_t nonce = 0;
+    
     uint8_t resultHash1[SHA512_DIGEST_LENGTH];
     uint8_t resultHash2[SHA512_DIGEST_LENGTH];
     
-    for (int i = 0; i < 1000000; i++)
+    while (trialValue > target)
     {
         nonce++;
         memcpy(buffer, &nonce, 8);
         SHA512(buffer, SHA512_DIGEST_LENGTH + 8, resultHash1);
         SHA512(resultHash1, SHA512_DIGEST_LENGTH, resultHash2);
+        memcpy(&trialValue, resultHash2, sizeof(trialValue));
+        trialValue = htonll(trialValue);
     }
-
+    return nonce;
  }
 
 int main(int argc, char** argv)
@@ -61,12 +62,17 @@ int main(int argc, char** argv)
 
     int long_index =0;
     int opt;
-
+    char* initial_hash_str = 0;
+    uint64_t target_val = 0;
+    char initial_hash[SHA512_DIGEST_LENGTH];
+    
     while ((opt = getopt_long(argc, argv, "t:i:h", long_options, &long_index )) != -1) {
         switch (opt) {
         case 't':
+                target_val = strtoull(optarg,0,10);
             break;
         case 'i':
+                initial_hash_str = optarg;
             break;
         case 'h':
             print_usage();
@@ -78,6 +84,39 @@ int main(int argc, char** argv)
             break;
         }
     }
+    
+    if (target_val == 0)
+    {
+        printf("ERROR: you must specify a target value.\n");
+        exit (EXIT_FAILURE);
+    }
 
+    if (initial_hash_str == 0)
+    {
+        printf("ERROR: you must specify a hash string.\n");
+        exit (EXIT_FAILURE);
+    }
+
+    if (strlen(initial_hash_str) != SHA512_DIGEST_LENGTH*2)
+    {
+        printf("ERROR: the initial hash string must have %d digits.\n",SHA512_DIGEST_LENGTH*2);
+        exit (EXIT_FAILURE);
+    }
+    
+    char *pos = initial_hash_str;
+
+    while( *pos )
+    {
+        if( !((pos-initial_hash_str)&1) )
+        {
+            unsigned int i;
+            sscanf(pos,"%02x",&i);
+            initial_hash[(pos- initial_hash_str)>>1] = i;
+        }
+        ++pos;
+    }
+    
+    printf("%lld\n",doMHash(target_val, initial_hash));
+    
     return EXIT_SUCCESS;
 }
